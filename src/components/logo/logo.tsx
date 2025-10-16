daff7bdecab1679133d872e659c5937c8d62db30
Cyecaid <loginmish@gmail.com>
2025-10-16 16:10:21 +0500

    fixes from previous review

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,13 +1,12 @@
 import {Link} from 'react-router-dom';
+import React from 'react';
 
-function Logo() : JSX.Element {
-  return (
-    <div className="header__left">
-      <Link className="header__logo-link" to="/">
-        <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-      </Link>
-    </div>
-  );
-}
+const Logo: React.FC = () => (
+  <div className="header__left">
+    <Link className="header__logo-link" to="/">
+      <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
+    </Link>
+  </div>
+);
 
 export default Logo;
